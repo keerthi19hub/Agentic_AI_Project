349479faa07d04e6d5accdf9b8abde7e1b7d82b1
keerthi19hub <nkeerthiBCA24@ced.alliance.edu.in>
2025-12-22 17:50:07 +0530

    Add files via upload

---
+++ Time_Management_Agent/gemini_llm.py
@@ -0,0 +1,11 @@
+from google import genai
+import os
+def gemini_generate_plan(prompt):
+    api_key = os.getenv("GOOGLE_API_KEY")
+    if not api_key:
+        return "Gemini API key not configured"
+
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt)
+    return response.text
